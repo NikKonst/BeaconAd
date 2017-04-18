a118516dfa145fcd2b571495855338a309780115
NikKonst <nikitakonst@mail.ru>
2017-04-18 19:20:40 +0300

    Project and dependencies set up

---
+++ BeaconAdSDK/BeaconAd.swift
@@ -0,0 +1,23 @@
+//
+//  BeaconAd.swift
+//  BeaconAdSDK
+//
+//  Created by Nikita Konstantinovskiy on 18.04.17.
+//  Copyright © 2017 BeaconAd. All rights reserved.
+//
+
+import Foundation
+import ZXingObjC
+
+public class BeaconAd {
+    let UID: String
+    
+    init(companyUID: String) {
+        UID = companyUID
+    }
+    
+    public func GetBeacons() -> String {
+        return "FUCK"
+    }
+}
+
